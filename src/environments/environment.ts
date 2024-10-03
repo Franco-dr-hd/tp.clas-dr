50341cf561eda6e81e2ae6a3b0361a9b5e3590f5
Franco-dr-hd <derenzisjeanfranco@gmail.com>
2024-10-03 10:28:50 -0300

    Vinculación con stronge completada; métodos modificados en 'table.ts' e inputs en 'table.html' | CRUD finalizado

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+// CONFIGURACIÓN DE FIREBASE CON NUESTRO PROYECTO -> VINCULAR
+export const environment = {
+    production: false,
+
+    // sacamos palabra "const" y ";" final y cambiamos el "=" por ":"
+     firebaseConfig: {
+        apiKey: "AIzaSyAW5UkbpFZejWOvfX2_3Pg4Fr-9u_CYp5U",
+        authDomain: "proyedr-bea49.firebaseapp.com",
+        projectId: "proyedr-bea49",
+        storageBucket: "proyedr-bea49.appspot.com",
+        messagingSenderId: "744376714634",
+        appId: "1:744376714634:web:8d73909f0a04ca96f4b473"
+    }
+}
